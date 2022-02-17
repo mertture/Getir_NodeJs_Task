b0b5d1c4ecf2a9696d178494b1e5f44d5a40000f
Mert Türe <mertture0@gmail.com>
2022-02-17 18:15:38 +0300

    fix port number

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ app.use("", (req, res) => {
 
 
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server is running on port number ${port}`))
+app.listen(process.env.PORT || 5000, () => console.log(`Server is running on port number ${process.env.PORT || 5000}`))
